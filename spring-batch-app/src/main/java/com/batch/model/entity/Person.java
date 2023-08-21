67730a0c25656362e5aec1a51e6115674151fb82
axelccp <hpmajin@outlook.es>
2023-08-20 20:27:35 -0600

    fix properties y entity

--- spring-batch-app/src/main/java/com/batch/model/entity/Person.java
+++ spring-batch-app/src/main/java/com/batch/model/entity/Person.java
@@ -1,10 +1,12 @@
 package com.batch.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.Data;
 
 
 import javax.persistence.*;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
 @Entity
 @Table(name="persons") @Data
 public class Person {
